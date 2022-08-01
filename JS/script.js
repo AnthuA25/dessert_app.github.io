a57f2843a2e9476e6efd865fa4a21cee22dbc652
Aylin <aylin_santacruz@hotmail.com>
2022-07-31 23:36:58 -0500

    add DOM

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,67 @@
 
-function Producto(nombre, precio, stock) {
+// const ham = document.querySelector('.ham');
+// const enlaces = document.querySelector('.link-menu');
+
+// ham.addEventListener('click', () => {
+//     enlaces.classList.toggle('activado');
+// })
+
+const cart = document.getElementById('cart-shopping-icons');
+// console.log(cart)
+const order = document.querySelector('.shopping-cart')
+console.log(order)
+
+cart.addEventListener('click',() =>{
+    order.classList.toggle('activado')
+})
+
+function Producto(nombre, precio, stock, imagen) {
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
+    this.imagen = imagen;
 };
 
-const productoA = new Producto("Pie de Limon", 45, 300);
-const productoB = new Producto("Selva Negra", 38, 400);
-const productoC = new Producto("Cupcakes", 38, 100);
-const productoD = new Producto("Brownies", 25, 500)
+const productoA = new Producto("Pie de Limon", 45, 300, "/image/biscocho.jpg");
+const productoB = new Producto("Selva Negra", 38, 400, "/image/biscocho.jpg");
+const productoC = new Producto("Cupcakes", 38, 100, "/image/biscocho.jpg");
+const productoD = new Producto("Alfajores", 25, 500, "/image/biscocho.jpg")
+const productoE = new Producto("Pie de Manzana", 25, 500, "/image/biscocho.jpg")
+const productoF = new Producto("Brownies", 25, 500, "/image/biscocho.jpg")
+const productoG = new Producto("Brownies", 25, 500, "/image/biscocho.jpg")
+const productoH = new Producto("Brownies", 25, 500, "/image/biscocho.jpg")
+const productoI = new Producto("Brownies", 25, 500, "/image/biscocho.jpg")
 
 let listaProductos = [
     productoA,
     productoB,
     productoC,
-    productoD
+    productoD,
+    productoE,
+    productoF,
+    productoG,
+    productoH,
+    productoI
 ]
 
+
+for (const producto of listaProductos) {
+    if (producto.stock != 0) {
+        
+        let container = document.getElementById('store')
+        let card = document.createElement('div');
+        let detail = document.createElement('div')
+        card.className = 'box-product';
+        container.appendChild(card)
+        card.innerHTML =`<a href="#" class="fas fa-heart"></a>
+        <img src=${producto.imagen} alt="" />`
+        card.appendChild(detail);
+        detail.className = 'details'
+        detail.innerHTML =` <h3>${producto.nombre}</h3>
+        <span>S/.${producto.precio}</span><br/>
+        <button class="button-store">Agregar a Carrito</button>`
+    }
+}
 
 let nombresProductos = [];
 
@@ -27,7 +72,7 @@ const listarProductos = () => {
 }
 listarProductos();
 
-let cantidadAComprar = prompt("Ingrese cantidad de productos a comprar");
+// let cantidadAComprar = prompt("Ingrese cantidad de productos a comprar");
 let precioTotal = 0;
 
 function calcularPrecio(cantidad, precio) {
@@ -46,8 +91,8 @@ function calcularStock(cantidad, producto) {
 let productoSeleccionado = [];
 
 for (let i = 0; i < cantidadAComprar; i++) {
-    var compraProducto = prompt("Ingrese nombre del producto:\n " + nombresProductos.join("\n "));
-    let cantidadProductos = prompt("Ingrese cantidad de productos a comprar");
+    // var compraProducto = prompt("Ingrese nombre del producto:\n " + nombresProductos.join("\n "));
+    // let cantidadProductos = prompt("Ingrese cantidad de productos a comprar");
     productoSeleccionado.push(compraProducto);
     if (compraProducto === productoA.nombre) {
         calcularStock(cantidadProductos, productoA);
@@ -68,7 +113,7 @@ alert(`Productos seleccionados :\n ${productoSeleccionado} \n  Este es el precio
 
 const buscarProducto = () => {
     let busqueda = prompt("Buscar Producto");
-    let busquedaProducto = listaProductos.find(elemento => elemento.nombre === busqueda);
+    let busquedaProducto = listaProductos.find(producto => producto.nombre === busqueda);
     return busquedaProducto
     // console.log(busquedaProducto);
 }
@@ -78,9 +123,8 @@ console.log(buscarProducto());
 const filtrarPrecio = () => {
     let filtrar = parseInt(prompt("Ingrese el precio a buscar"))
 
-    let busquedaPrecio = listaProductos.filter(elemento => elemento.precio === filtrar)
+    let busquedaPrecio = listaProductos.filter(producto => producto.precio === filtrar)
     return busquedaPrecio
-    // console.log(busquedaPrecio)
 }
 
 console.log(filtrarPrecio())
